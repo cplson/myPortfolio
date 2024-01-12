fcb63abe11631fac8e1071becefefb990bd301ec
James Jacobsen <edjcjacobsen@gmail.com>
2024-01-12 15:49:03 -0600

    finished styling and functionality for the contact form

--- app.js
+++ app.js
@@ -11,14 +11,19 @@ window.addEventListener("DOMContentLoaded", async () => {
         // generate a five digit number for the contact_number variable
         this.contact_number.value = (Math.random() * 100000) | 0;
         // these IDs from the previous steps
-        emailjs.sendForm("contact_service", "contact_form", this).then(
-          function () {
-            console.log("SUCCESS!");
-          },
-          function (error) {
-            console.log("FAILED...", error);
-          }
-        );
+        alert("success, yay!");
+        // emailjs.sendForm("contact_service", "contact_form", this).then(
+        //   function () {
+        //     console.log("SUCCESS!");
+        //   },
+        //   function (error) {
+        //     console.log("FAILED...", error);
+        //   }
+        // );
+
+        document.getElementById("contact-form").style.display = "none";
+        document.getElementById("contact-button").style.display = "block";
+
         let name = document.getElementsByName("user_name");
         let email = document.getElementsByName("user_email");
         let message = document.getElementsByName("message");
@@ -28,4 +33,13 @@ window.addEventListener("DOMContentLoaded", async () => {
         message[0].value = "";
       });
   };
+
+  document
+    .getElementById("contact-button")
+    .addEventListener("click", displayContactForm);
 });
+
+const displayContactForm = () => {
+  document.getElementById("contact-button").style.display = "none";
+  document.getElementById("contact-form").style.display = "block";
+};
