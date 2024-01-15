3627452e41108d8574c908e85bad743d124da545
James Jacobsen <edjcjacobsen@gmail.com>
2024-01-15 14:07:44 -0600

    fixed different errors after different fixed chained problems

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ window.addEventListener("DOMContentLoaded", async () => {
       .getElementById("contact-form")
       .addEventListener("submit", function (event) {
         event.preventDefault();
+
+        loadToastSuccess();
+        loadToastFail();
         // generate a five digit number for the contact_number variable
         this.contact_number.value = (Math.random() * 100000) | 0;
         // these IDs from the previous steps
@@ -19,14 +22,16 @@ window.addEventListener("DOMContentLoaded", async () => {
         let email = document.getElementsByName("user_email");
         let message = document.getElementsByName("message");
 
-        emailjs.sendForm("contact_service", "contact_form", this).then(
-          function () {
-            showToastSuccess();
-          },
-          function (error) {
-            showToastFail();
-          }
-        );
+        // emailjs.sendForm("contact_service", "contact_form", this).then(
+        //   function () {
+        //     showToastSuccess();
+        //   },
+        //   function (error) {
+        //     showToastFail();
+        //   }
+        // );
+
+        showToastSuccess();
 
         document.getElementById("contact-form").style.height = "0px";
         document.getElementById("contact-form").style.opacity = "0";
@@ -48,9 +53,9 @@ window.addEventListener("DOMContentLoaded", async () => {
 const displayContactForm = () => {
   document.getElementById("contact-form").style.height = "auto";
   document.getElementById("contact-button").style.display = "none";
-  loadToastSuccess();
-  loadToastFail();
-  document.getElementById("contact-form").style.opacity = "1";
+  setTimeout(() => {
+    document.getElementById("contact-form").style.opacity = "1";
+  }, 50);
 };
 
 const loadToastSuccess = () => {
@@ -69,24 +74,28 @@ const loadToastFail = () => {
 
 const showToastSuccess = () => {
   let container = document.getElementById("contact-wrapper");
-  let toast = document.getElementById("toast-success");
+  let toast = document.getElementById("toast-fail");
+  container.removeChild(toast);
+  toast = document.getElementById("toast-success");
   toast.style.right = "8px";
   setTimeout(() => {
     toast.style.opacity = "0";
     setTimeout(() => {
       container.removeChild(toast);
-    }, 1600);
+    }, 1000);
   }, 3000);
 };
 
 const showToastFail = () => {
   let container = document.getElementById("contact-wrapper");
-  let toast = document.getElementById("toast-fail");
+  let toast = document.getElementById("toast-success");
+  container.removeChild(toast);
+  toast = document.getElementById("toast-fail");
   toast.style.right = "8px";
   setTimeout(() => {
     toast.style.opacity = "0";
     setTimeout(() => {
       container.removeChild(toast);
-    }, 1600);
+    }, 1000);
   }, 3000);
 };
