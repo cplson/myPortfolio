79c183d99cadaf205182c3337b48356945766911
James Jacobsen <edjcjacobsen@gmail.com>
2024-01-18 13:49:24 -0600

    got a testimonial on the page using some hard coding

--- components/Testimonial.js
+++ components/Testimonial.js
@@ -1,10 +1,20 @@
 import { fetchTestimonials } from "../services/API.js";
 
+const renderTestimonials = async () => {
+  const testimonialList = await fetchTestimonials.getTestimonials();
+  const template = document.getElementById("testimonial-template");
+  const container = document.getElementById("testimonial-wrapper");
+  let content = template.content.cloneNode(true);
+  container.appendChild(content);
+  content = document.getElementsByClassName("testimonial");
+  document.getElementsByClassName("testimonial-img")[0].src =
+    testimonialList[0].img;
+  document.getElementsByClassName("testimonial-name")[0].innerHTML =
+    testimonialList[0].name;
+  document.getElementsByClassName("testimonial-relation")[0].innerHTML =
+    testimonialList[0].relation;
+  document.getElementsByClassName("testimony")[0].innerHTML =
+    testimonialList[0].testimony;
+};
 
-    const renderTestimonials = async () => {
-        const testimonialList = await fetchTestimonials.getTestimonials();
-        console.log(testimonialList);
-    }
-
-
-export default renderTestimonials
+export default renderTestimonials;
