97774ad60a8930dfa063d0dc00dcd1df4f1f6f7d
James Jacobsen <edjcjacobsen@gmail.com>
2024-01-18 19:43:39 -0600

    styled the mobile testimonial component

--- components/Testimonial.js
+++ components/Testimonial.js
@@ -1,20 +1,37 @@
 import { fetchTestimonials } from "../services/API.js";
-
+let activeTestimonial;
 const renderTestimonials = async () => {
   const testimonialList = await fetchTestimonials.getTestimonials();
-  const template = document.getElementById("testimonial-template");
-  const container = document.getElementById("testimonial-wrapper");
-  let content = template.content.cloneNode(true);
-  container.appendChild(content);
-  content = document.getElementsByClassName("testimonial");
-  document.getElementsByClassName("testimonial-img")[0].src =
-    testimonialList[0].img;
-  document.getElementsByClassName("testimonial-name")[0].innerHTML =
-    testimonialList[0].name;
-  document.getElementsByClassName("testimonial-relation")[0].innerHTML =
-    testimonialList[0].relation;
-  document.getElementsByClassName("testimony")[0].innerHTML =
-    testimonialList[0].testimony;
+  for (let i = 0; i < testimonialList.length; i++) {
+    let template = document.getElementById("testimonial-template");
+    let container = document.getElementById("testimonial-group");
+    let content = template.content.cloneNode(true);
+    container.appendChild(content);
+    content = document.getElementsByClassName("testimonial-card");
+    content[i].classList.add("hide");
+    document.getElementsByClassName("testimonial-img")[i].src =
+      testimonialList[i].img;
+    document.getElementsByClassName("testimonial-name")[i].innerHTML =
+      testimonialList[i].name;
+    document.getElementsByClassName("testimonial-relation")[i].innerHTML =
+      testimonialList[i].relation;
+    document.getElementsByClassName("testimony")[i].innerHTML =
+      testimonialList[i].testimony;
+
+    template = document.getElementById("testimonial-indicator-template");
+    container = document.getElementById("testimonial-indicator-group");
+    content = template.content.cloneNode(true);
+    container.appendChild(content);
+  }
+  if (testimonialList.length > 0) {
+      let testimonial = document.getElementsByClassName("testimonial-card");
+      activeTestimonial = testimonial[0];
+    testimonial[0].classList.remove("hide");
+    testimonial[0].classList.add("show");
+    let indicators = document.getElementsByClassName("testimonial-indicator");
+    indicators[0].classList.remove("inactive");
+    indicators[0].classList.add("active");
+  }
 };
 
 export default renderTestimonials;
